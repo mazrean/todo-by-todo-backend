c50a40585a04e5f4d960f5d80b2b01d718d607c1
Hiroto Toyoda <hirototoyoda20020927@gmail.com>
2025-07-19 14:04:16 +0900

    ハンドラ関数のレシーバ名を修正

--- go/internal/router/todo.go
+++ go/internal/router/todo.go
@@ -26,15 +26,15 @@ type TodoRequest struct {
 	Done  bool   `json:"done"`
 }
 
-func (e *Todo) Handler(w http.ResponseWriter, r *http.Request) {
+func (t *Todo) Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
-func (e *Todo) GetTodoListHandler(w http.ResponseWriter, r *http.Request) {
+func (t *Todo) GetTodoListHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "GetTodoList, %q", html.EscapeString(r.URL.Path))
 }
 
-func (e *Todo) PostTodoHandler(w http.ResponseWriter, r *http.Request) {
+func (t *Todo) PostTodoHandler(w http.ResponseWriter, r *http.Request) {
 	var request TodoRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
@@ -43,7 +43,7 @@ func (e *Todo) PostTodoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "PostTodo: title=%s, done=%v", request.Title, request.Done)
 }
 
-func (e *Todo) UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
+func (t *Todo) UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 	var request TodoRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
@@ -53,7 +53,7 @@ func (e *Todo) UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "UpdateTodo id=%s: title=%s, done=%v", id, request.Title, request.Done)
 }
 
-func (e *Todo) DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {
+func (t *Todo) DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 	fmt.Fprintf(w, "DeleteTodo %s, %q", id, html.EscapeString(r.URL.Path))
 }
