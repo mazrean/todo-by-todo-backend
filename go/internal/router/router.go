58999002efc832d336330615c9620fccf0cb6fac
Hiroto Toyoda <hirototoyoda20020927@gmail.com>
2025-07-19 12:39:15 +0900

    微修正

--- go/internal/router/router.go
+++ go/internal/router/router.go
@@ -24,14 +24,11 @@ type Addr string
 func (r *Router) Run() error {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/todo", func(w http.ResponseWriter, req *http.Request) {
 
 	mux.HandleFunc("GET /todo", r.todo.GetTodoListHandler)
 	mux.HandleFunc("POST /todo", r.todo.PostTodoHandler)
 	mux.HandleFunc("PUT /todo/{id}", r.todo.UpdateTodoHandler)
 	mux.HandleFunc("DELETE /todo/{id}", r.todo.DeleteTodoHandler)
-
-})
 
 	return http.ListenAndServe(
 		r.addr,
