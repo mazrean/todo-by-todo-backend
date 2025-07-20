9778e499fdd3d96b84f2b9ca10a966a8b8050970
Hiroto Toyoda <hirototoyoda20020927@gmail.com>
2025-07-20 13:12:20 +0900

    CreateUserメソッドのトランザクション処理を削除し、エラーハンドリングを改善

--- modules/todo/internal/router/user.go
+++ modules/todo/internal/router/user.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
 	todoapi "github.com/mazrean/todo-by-todo-backend/modules/todo/internal/bindings/todo/api/todo-api"
@@ -24,6 +25,13 @@ type UserRequest struct {
 }
 
 func (u *User) CreateUserHandler(request todoapi.UserRequest) (result cm.Option[todoapi.APIError]) {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("panic in CreateUserHandler", "panic", r)
+			result = cm.Some(todoapi.APIErrorInternalError(fmt.Sprintf("panic: %v", r)))
+			return
+		}
+	}()
 	ctx := context.Background()
 
 	_, err := u.userRepo.CreateUser(ctx, request.Name)
